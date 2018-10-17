1d5896aa2c8bd376d25cc000005ff6736435c098
mohamed amine tarhouni <1895088@bdeb.qc.ca>
2018-10-16 23:41:49 -0400

    TP1 .jar

---
+++ src/MesProcessus/Processus.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MesProcessus;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1895088
+ */
+public class Processus { 
+    String message="Saisir une date inferieur à 2010 ";
+    
+    public String processusActivite(String _date){
+        
+      
+      try{  
+        Date date = new Date(Integer.parseInt(_date));
+        
+            if(date.before(new Date(1971))){
+                message="Il est temps d’aller se promener à travers le monde";
+            }
+            else if(date.before(new Date(1981))){
+                message="il est temps de commencer à travailler  sérieusement";
+            }
+            else if(date.before(new Date(1991))){
+                message="Il est grand temps de terminer tes études ";
+            }
+            else if (date.before(new Date(2001))){
+                message="Fais ce qui te plait, tu as encore le temps! ";
+                
+            }
+            else if (date.before(new Date(2011))){
+                 message="utilisation de ce service non-autorisée !!! ";
+            }
+        return message;
+        
+      }catch(Exception e){
+            e.printStackTrace();
+            message="utilisation de ce service non-autorisée !!! ";
+          return message;
+      }
+    }
+}
